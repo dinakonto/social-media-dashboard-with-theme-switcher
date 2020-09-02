1a0ab6bc2930ccc04fa5c9b82ff5ecae2c3c0091
Dina Konto <dina.konto@gmail.com>
2020-09-03 00:08:02 +1000

    Add functioning theme switch toggle and label

--- js/theme-switcher.js
+++ js/theme-switcher.js
@@ -2,20 +2,32 @@
 function setTheme(themeName) {
   localStorage.setItem('theme', themeName);
   document.documentElement.className = themeName;
+  let label = document.getElementById('current-theme')
+  label.innerText = themeName
+}
+
+// Set label
+function setLabel(label) {
+  let div = document.getElementById('current-theme')
+  div.innerText = label
 }
 
 // Toggle between light and dark theme
 function toggleTheme() {
   if (localStorage.getItem('theme') === 'theme-dark') {
     setTheme('theme-light');
+    setLabel('Light Mode');
   } else {
     setTheme('theme-dark');
+    setLabel('Dark Mode');
   }
 }
 
 // Set theme on initial load
 if (localStorage.getItem('theme') === 'theme-dark') {
   setTheme('theme-dark');
+  setLabel('Dark Mode');
 } else {
   setTheme('theme-light');
+  setLabel('Light Mode');
 }
